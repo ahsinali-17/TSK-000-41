e41e5a734a6310739710323143b9bba189ed51df
ahsinali-17 <aajoker17@gmail.com>
2024-06-02 15:45:09 +0500

    task 02 completed

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import First from './components/First'
+import Second from './components/Second'
+import Third from './components/Third'
+import Fourth from './components/Fourth'
+import Fifth from './components/Fifth'
+import Footer from './components/Footer'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Login from './components/Login'
+
+function App() {
+  let router = createBrowserRouter([
+    {
+      path:'/',
+      element:
+      <>
+      <div className='internee max-w-[100vw] overflow-hidden'>
+      <Navbar/>
+       <First />
+       <Second/>
+       <Third />
+       <Fourth/>
+       <Fifth />
+       <Footer/>
+       </div>
+       </>  
+   },
+    {
+       path:'/login',
+       element:<Login/>
+    },
+])
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
